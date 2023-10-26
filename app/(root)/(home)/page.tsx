ba3523ff77b59ebf17c4387d554829a57956d810
Juan Cruz Caceres <caceresjuan.dev@gmail.com>
2023-10-25 21:23:42 -0300

    implement initial routing folders

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold">Home</h1>
+    </div>
+  );
+}
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next.js 13.5 Course</h1>
-    </div>
-  );
-}
