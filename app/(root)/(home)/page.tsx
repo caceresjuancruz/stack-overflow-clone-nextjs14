ee313c8b0be0e52c00e15919694bf8b5f6013691
Juan Cruz Caceres <caceresjuan.dev@gmail.com>
2023-10-27 09:35:32 -0300

    implement navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">Home</h1>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
