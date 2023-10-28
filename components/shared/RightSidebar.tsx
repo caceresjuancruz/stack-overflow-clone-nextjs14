8411f463803eedc1e09beb7b469a13fb667f3e96
Juan Cruz Caceres <caceresjuan.dev@gmail.com>
2023-10-28 18:07:50 -0300

    implement right sidebar static ui content

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,10 +1,72 @@
+import Link from "next/link";
 import UserMenu from "./navbar/UserMenu";
+import Image from "next/image";
+import RenderTag from "./RenderTag";
+
+const hotQuestions = [
+  { _id: 1, title: "How do I use express as a custom server in NextJS?" },
+  { _id: 2, title: "Cascading Deletes in SQLAlchemy?" },
+  { _id: 3, title: "How to Perfectly Center a Div with Tailwind CSS?" },
+  {
+    _id: 4,
+    title:
+      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
+  },
+  { _id: 5, title: "Redux Toolkit Not Updating State as Expected" },
+];
+
+const popularTags = [
+  { _id: 1, name: "javascript", totalQuestions: 5 },
+  { _id: 2, name: "react", totalQuestions: 5 },
+  { _id: 3, name: "next", totalQuestions: 5 },
+  { _id: 4, name: "vue", totalQuestions: 2 },
+  { _id: 5, name: "redux", totalQuestions: 10 },
+];
 
 const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar shadow-light100_dark100 sticky right-0 top-0 z-40 flex h-screen flex-col justify-between overflow-y-auto border-l px-6 pb-12 pt-6 max-xl:hidden lg:z-50 lg:w-[266px]">
       <div>
         <UserMenu />
+      </div>
+      <div className="">
+        <div>
+          <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+          <div className="mt-7 flex w-full flex-col gap-[30px]">
+            {hotQuestions.map((question) => (
+              <Link
+                href={`/questions/${question._id}`}
+                key={question._id}
+                className="flex cursor-pointer items-center justify-between gap-7"
+              >
+                <p className="body-medium text-dark500_light700">
+                  {question.title}
+                </p>
+                <Image
+                  src="/assets/icons/chevron-right.svg"
+                  alt="chevron"
+                  width={20}
+                  height={20}
+                  className="invert-colors"
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="mt-16">
+          <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+          <div className="mt-7 flex flex-col gap-4">
+            {popularTags.map((tag) => (
+              <RenderTag
+                key={tag._id}
+                _id={tag._id}
+                name={tag.name}
+                totalQuestions={tag.totalQuestions}
+                showCount
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
