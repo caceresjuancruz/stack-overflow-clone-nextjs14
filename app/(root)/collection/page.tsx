0e7501778a1a06a666d4cca54d7dd63a0ba83219
Juan Cruz Caceres <caceresjuan.dev@gmail.com>
2023-11-05 02:14:26 -0300

    implement collections page

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,60 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import UserCard from "@/components/cards/UserCard";
+import Filter from "@/components/shared/Filter";
+import NoResults from "@/components/shared/NoResults";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+
+export default async function CollectionsPage() {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({ clerkId: userId as string });
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          placeholder="Search for saved questions"
+          iconPosition="left"
+          iconSrc="/assets/icons/search.svg"
+          route="/collection"
+        />
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <section className="mt-10 flex w-full flex-col gap-6">
+        {result.questions && result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResults
+            title="No Saved Questions Found"
+            description="It appears that there are no saved questions in your collection at the moment 😔.Start exploring and saving questions that pique your interest 🌟"
+            link="/"
+            linkTitle="Explore questions"
+          />
+        )}
+      </section>
+    </div>
+  );
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,12 +6,15 @@ import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   GetUserByIdParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/models/question.model";
 import { getErrorMessage } from "../utils";
+import Tag from "@/database/models/tag.model";
+import { FilterQuery } from "mongoose";
 
 export async function getAllUsers(params: GetAllUsersParams) {
   const { page = 1, pageSize = 10, searchQuery, filter } = params;
@@ -104,6 +107,39 @@ export async function deleteUser(params: DeleteUserParams) {
     const deletedUser = await User.findByIdAndDelete(user._id);
 
     return deletedUser;
+  } catch (error) {
+    return {
+      message: getErrorMessage(error),
+    };
+  }
+}
+
+export async function getSavedQuestions(params: GetSavedQuestionsParams) {
+  const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+  try {
+    connectToDatabase();
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, "i") } }
+      : {};
+
+    const user = await User.findOne({ clerkId }).populate({
+      path: "saved",
+      match: query,
+      options: { sort: { createdAt: -1 } },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name avatar" },
+      ],
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const savedQuestions = user.saved;
+
+    return { questions: savedQuestions };
   } catch (error) {
     return {
       message: getErrorMessage(error),
