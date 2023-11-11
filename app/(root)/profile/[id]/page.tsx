c72689b9b8715063dc345d140e0be598ff852cd3
Juan Cruz Caceres <caceresjuan.dev@gmail.com>
2023-11-10 22:13:31 -0300

    implement Stats component to profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -75,7 +75,10 @@ export default async function ProfilePage({ params, searchParams }: URLProps) {
           </SignedIn>
         </div>
       </div>
-      <Stats />
+      <Stats
+        totalQuestions={result.totalQuestions!}
+        totalAnswers={result.totalAnswers!}
+      />
       <div className="mt-10 flex gap-10">
         <Tabs defaultValue="top-posts" className="flex-1">
           <TabsList className="background-light800_dark400 min-h-[42px] p-1">
